fdd1e3824dcc096c0287f93755f88eb1864ae251
ChinoISC <oscar010394@gmail.com>
2024-10-20 14:32:34 -0600

    Login Register Home

--- api/login/login_view.py
+++ api/login/login_view.py
@@ -1,7 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+# View for Login.
 def login_views(request):
     template_name = "auth-login.html"
     
+        # Verifica si el usuario ya está autenticado
+    if request.user.is_authenticated and request.user.is_active:
+        return redirect('home')
+    
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+                login(request, user)
+                return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials or User is not active')
     return render(request,template_name)
+
+# View for Register
+def register_view(request):
+    template_name = "auth-register.html"
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+        
+        if password != password_confirmation:
+            messages.error(request, 'Las contraseñas no coinciden')
+            return render(request, template_name)
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'El usuario ya existe')
+            return render(request, template_name)
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'El correo ya existe')
+            return render(request, template_name)
+
+        user = User(
+            username=username,
+            email=email,
+            password=make_password(password),
+            is_active = 0
+        )
+        user.save()
+        messages.success(request, 'Cuenta creada exitosamente')
+    return render(request,template_name)
+
+# View for Forgot the Password
+def forgot_view(request):
+    template_name = "auth-forgot-password.html"
+    return render(request,template_name)
+
+# View for logout.
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
